0cea22afe799c7260eb9cffeb7715f35a1ddcb24
MBJ <thiswallz@gmail.com>
2022-11-21 11:06:35 -0300

    standard actions

--- packages/react/lib/types/icons/standard-context/MediaSeekForward.d.ts
+++ packages/react/lib/types/icons/standard-context/MediaSeekForward.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 import { IconProps } from '../../util';
 declare const _default: ({ theme, size, primary, secondary, border, stroke, className, onClick, }: IconProps) => JSX.Element;
 export default _default;
